e68ee94b3e1935c52057b199a90459b2ff696821
FogotUs <62462064+FogotUs@users.noreply.github.com>
2023-11-03 13:59:52 +0700

    restruct code in main

--- public/index.php
+++ public/index.php
@@ -2,5 +2,6 @@
 declare(strict_types = 1);
 include_once("../vendor/autoload.php");
 use App\Parser;
-    var_dump(Parser::highlight_nicknames("Я живу в одном доме с @300spartans и с @user " ));
+    $nickForValidate = "@user ";
+    $validNick = Parser::highlight_nicknames( $nickForValidate ));
 ?>
